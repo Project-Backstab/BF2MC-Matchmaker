74d715d2abafaaa7c9881b143eb83b7332d7cb6f
Ludo Ruisch <admin@ludoruisch.nl>
2023-06-02 03:56:45 +0000

    add: Server browser can now return server lists, first concept.

--- src/browsing/client.h
+++ src/browsing/client.h
@@ -4,6 +4,18 @@
 #include <gamespy.h>
 #include <net/socket.h>
 
+//message types for outgoing requests
+#define SERVER_LIST_REQUEST   0
+#define SERVER_INFO_REQUEST   1
+#define SEND_MESSAGE_REQUEST  2
+#define KEEPALIVE_REPLY       3
+#define MAPLOOP_REQUEST       4
+#define PLAYERSEARCH_REQUEST  5
+
+#define CRYPTCHAL_LEN         10
+#define SERVCHAL_LEN          25
+#define LIST_CHALLENGE_LEN    8
+
 namespace Browsing
 {
 	class Client : public Net::Socket
@@ -18,12 +30,12 @@ namespace Browsing
 			/*
 				Events
 			*/
-			void onRequest(const std::string &msg);
+			void onRequest(const std::vector<unsigned char>& msg);
 			
 			/*
 				Requests
 			*/
-			void requestServerList(const GameSpy::Parameter &parameter);
+			void requestServerList(const std::vector<unsigned char>& msg);
 			
 		private:
 			void _LogTransaction(const std::string &direction, const std::string &response) const;
