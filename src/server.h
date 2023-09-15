7db55c81b34beea4e7847e3be6926ed47c2b6c63
Ludo Ruisch <admin@ludoruisch.nl>
2023-09-15 19:52:44 +0000

    patch: cleanup.

--- src/server.h
+++ src/server.h
@@ -10,12 +10,12 @@ class Server : public Net::Socket
 	public:
 		enum Type
 		{
-			QR,				// udp:27900 domain: bfield1942ps2.master.bfmcspy.net
-			GPSP,			// tcp:29901 domain: gpsp.gamespy.com
-			GPCM,			// tcp:29900 domain: gpcm.gamespy.com
-			Webserver,		// tcp:80    domain: www.easports.com and bfmc.gamespy.com
-			Browsing,       // tcp:28910 domain: bfield1942ps2.available.gamespy.com and bfield1942ps2.ms7.gamespy.com
-			GameStats,		// tcp:29920 domain: bfield1942ps2.gamestats.bfmcspy.net
+			QR,        // udp:27900 domain: bfield1942ps2.master.bfmcspy.net
+			GPSP,      // tcp:29901 domain: gpsp.gamespy.com
+			GPCM,      // tcp:29900 domain: gpcm.gamespy.com
+			Webserver, // tcp:80    domain: www.easports.com and bfmc.gamespy.com
+			Browsing,  // tcp:28910 domain: bfield1942ps2.available.gamespy.com and bfield1942ps2.ms7.gamespy.com
+			GameStats, // tcp:29920 domain: bfield1942ps2.gamestats.bfmcspy.net
 			None,
 		};
 	
